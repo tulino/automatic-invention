f5724421de988d46ea91e3dd8447f7bcb0874e1d
tulin ozen <tulin.ozen@bil.omu.edu.tr>
2017-05-01 12:01:06 +0300

    Algoritma eklendi

--- binary_tree.rb
+++ binary_tree.rb
@@ -1,2 +1,14 @@
+class BinaryTree
 
-#algoritma gelecek
+  def self.on(grid)
+    grid.each_cell do |cell|
+      neighbors = []
+      neighbors << cell.north if cell.north
+      neighbors << cell.east if cell.east
+
+      index = rand(neighbors.length)
+      neighbor = neighbors[index]
+      cell.link(neighbor) if neighbor
+    end
+  end
+end
